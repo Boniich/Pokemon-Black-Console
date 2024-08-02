e25230165452354991c00fd26889bbd57e322a17
Boniich <ezequieldbo25@gmail.com>
2024-08-02 14:50:31 -0300

    feat: let all ready to implement turn system

--- PokemonBlackConsole/game/battleSystem/BattleSystem.cpp
+++ PokemonBlackConsole/game/battleSystem/BattleSystem.cpp
@@ -3,12 +3,24 @@
 void BattleSystem::startBattle(Pokemon& attacker, Pokemon& defender) {
 	do {
 	
+		//mi turno
+		if (isPokemonAlive(defender)) {
+			std::cout << "El pokemon rival fue derrotado. Tu ganas!" << std::endl;
+			this->endBattle = true;
+		}
+		else {
+			//rivalTurn
+			if (isPokemonAlive(attacker)) {
+				std::cout << "El pokemon alaido fue derrotado. Tu pierdes!" << std::endl;
+				this->endBattle = true;
+			}
+		}
 	} while (!this->endBattle);
 }
 
 bool BattleSystem::isPokemonAlive(Pokemon pokemon) {
-	if (pokemon.getPs() <= 0) {
-		return false;
+	if (pokemon.getPs() > 0) {
+		return true;
 	}
-	return true;
+	return false;
 }
--- PokemonBlackConsole/game/battleSystem/battleSystem.h
+++ PokemonBlackConsole/game/battleSystem/battleSystem.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../pokemon/Pokemon.h"
+#include <iostream>
 
 class Pokemon;
 
