47709f2a2a8683f5f637181281d0802c94bd77d6
Boniich <ezequieldbo25@gmail.com>
2024-08-02 21:50:46 -0300

    feat: add turn system instance

--- PokemonBlackConsole/game/battleSystem/battleSystem.h
+++ PokemonBlackConsole/game/battleSystem/battleSystem.h
@@ -1,11 +1,13 @@
 #pragma once
 #include "../pokemon/Pokemon.h"
+#include "./TurnSystem/TurnSystem.h"
 #include <iostream>
 
 class Pokemon;
 
 class BattleSystem {
 	//instancia del sistema de turnos
+	TurnSystem turnSystem;
 	bool endBattle = true;
 
 private:
