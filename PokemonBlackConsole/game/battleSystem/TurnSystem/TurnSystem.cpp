a07182625d92916213aad1eaaf204fa728caaf10
Boniich <ezequieldbo25@gmail.com>
2024-08-04 23:50:45 -0300

    feat: improve the code that handle the seleccion of an attack

--- PokemonBlackConsole/game/battleSystem/TurnSystem/TurnSystem.cpp
+++ PokemonBlackConsole/game/battleSystem/TurnSystem/TurnSystem.cpp
@@ -34,30 +34,47 @@ void TurnSystem::myTurn(Pokemon& attacker, Pokemon& defender) {
 		std::cout << "Introduce una opcion: " << std::endl;
 
 		std::cin >> opcion;
+		/*
+		cantidad ataques|rango de opciones para ingresar
+		1				| 1
+		2				| 1~2
+		3				| 1~3
+		4				| 1~4
 
-		switch (opcion)
-		{
-		case 1:
-			battle.makeDamage(attacker, defender, attacker.getAttackArray()[opcion-1]);
-			endTurn = true;
-			break;
-		case 2:
-			battle.makeDamage(attacker, defender, attacker.getAttackArray()[opcion-1]);
-			endTurn = true;
-			break;
-		case 3:
-			battle.makeDamage(attacker, defender, attacker.getAttackArray()[opcion-1]);
-			endTurn = true;
-			break;
-		case 4:
-			battle.makeDamage(attacker, defender, attacker.getAttackArray()[opcion-1]);
-			endTurn = true;
-			break;
+		*/
 
-		default:
+		if (opcion <= 0 || opcion > attacker.getLengthAttackArray()) {
+
 			std::cout << "Opcion no valida! Vuelva a seleccionar una opcion. " << std::endl;
-			break;
 		}
+		else {
+			battle.makeDamage(attacker, defender, attacker.getAttackArray()[opcion - 1]);
+			endTurn = true;
+		}
+
+		//switch (opcion)
+		//{
+		//case 1:
+		//	battle.makeDamage(attacker, defender, attacker.getAttackArray()[opcion-1]);
+		//	endTurn = true;
+		//	break;
+		//case 2:
+		//	battle.makeDamage(attacker, defender, attacker.getAttackArray()[opcion-1]);
+		//	endTurn = true;
+		//	break;
+		//case 3:
+		//	battle.makeDamage(attacker, defender, attacker.getAttackArray()[opcion-1]);
+		//	endTurn = true;
+		//	break;
+		//case 4:
+		//	battle.makeDamage(attacker, defender, attacker.getAttackArray()[opcion-1]);
+		//	endTurn = true;
+		//	break;
+
+		//default:
+		//	std::cout << "Opcion no valida! Vuelva a seleccionar una opcion. " << std::endl;
+		//	break;
+		//}
 
 
 
@@ -72,47 +89,47 @@ void TurnSystem::myTurn(Pokemon& attacker, Pokemon& defender) {
 void TurnSystem::rivalTurn(Pokemon& attacker, Pokemon& defender) {
 	int rango_bajo = 1;
 	int rango_alto = attacker.getLengthAttackArray();
-	std::cout << "El pokemon " << attacker.getName() << " tene " << rango_alto <<" ataques"<<std::endl;
+
 	std::cout << "------------------------------------------------" << std::endl;
 	std::cout << "---------- Turno del pokemon rival ------------" << std::endl;
 	std::cout << "------------------------------------------------" << std::endl;
 
-	Attack* attack = attacker.getAttackArray();
-	std::cout << attack[0].getAttackId();
-
-
 	std::cout << "" << std::endl;
 	std::cout << "" << std::endl;
-
 
 
 	std::cout << defender.getName() << " (Aliado) PS: " << defender.getPs() << std::endl;
 	std::cout << attacker.getName() << " (Enemigo) PS: " << attacker.getPs() << std::endl;
 
-	for (int i = 0; i < attacker.getLengthAttackArray(); i++) {
-		std::cout << (i + 1) << "- " << (attacker.getAttackArray()[i]).getName() << std::endl;
-	}
-
 	int opcion = rango_bajo + std::rand() % (rango_alto - rango_bajo + 1);
 
-	switch (opcion)
-	{
-	case 1:
-		battle.makeDamage(attacker, defender, attacker.getAttackArray()[opcion - 1]);
-		break;
-	case 2:
-		battle.makeDamage(attacker, defender, attacker.getAttackArray()[opcion-1]);
-		break;
-	case 3:
-		battle.makeDamage(attacker, defender, attacker.getAttackArray()[opcion-1]);
-		break;
-	case 4:
-		battle.makeDamage(attacker, defender, attacker.getAttackArray()[opcion-1]);
-		break;
-	default:
-		std::cout << "Ups! Algo salio mal al attackar! Consulte con su programador de confianza"<<std::endl;
-		break;
+	if (opcion <= 0 || opcion > attacker.getLengthAttackArray()) {
+
+		std::cout << "Ups! Algo salio mal al atacar! Consulte con su programador de confianza" << std::endl;
 	}
+	else {
+		battle.makeDamage(attacker, defender, attacker.getAttackArray()[opcion - 1]);
+	}
+
+
+	//switch (opcion)
+	//{
+	//case 1:
+	//	battle.makeDamage(attacker, defender, attacker.getAttackArray()[opcion - 1]);
+	//	break;
+	//case 2:
+	//	battle.makeDamage(attacker, defender, attacker.getAttackArray()[opcion-1]);
+	//	break;
+	//case 3:
+	//	battle.makeDamage(attacker, defender, attacker.getAttackArray()[opcion-1]);
+	//	break;
+	//case 4:
+	//	battle.makeDamage(attacker, defender, attacker.getAttackArray()[opcion-1]);
+	//	break;
+	//default:
+	//	std::cout << "Ups! Algo salio mal al attackar! Consulte con su programador de confianza"<<std::endl;
+	//	break;
+	//}
 
 
 }
