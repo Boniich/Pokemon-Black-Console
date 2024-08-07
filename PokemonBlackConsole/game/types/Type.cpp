dd3442a625a8d06b343481a617a15c7b4e89b20c
Boniich <ezequieldbo25@gmail.com>
2024-08-07 15:05:46 -0300

    feat: add new string to types

--- PokemonBlackConsole/game/types/Type.cpp
+++ PokemonBlackConsole/game/types/Type.cpp
@@ -12,6 +12,18 @@ std::string Type::getType() {
 	case Fuego: return "Fuego";
 	case Agua: return "Agua";
 	case Electrico: return "Electrico";
+	case Normal: return "Normal";
+	case Planta: return "Planta";
+	case Fantasma: return "Fantasma";
+	case Hielo: return "Hielo";
+	case Bicho: return "Bicho";
+	case Roca: return "Roca";
+	case Dragon: return "Dragon";
+	case Lucha: return "Lucha";
+	case Tierra: return "Tierra";
+	case Veneno: return "Veneno";
+	case Volador: return "Volador";
+	case Psiquico: return "Psiquico";
 	default:
 		"No pertenece a ningun tipo definido";
 		break;
